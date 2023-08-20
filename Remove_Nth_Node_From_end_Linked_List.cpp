7f93760fc3608442e00bb07b143beb24c8e1e752
Sawan166 <114352257+Sawan166@users.noreply.github.com>
2023-08-20 11:46:42 +0530

    Create Remove_Nth_Node_From_end_Linked_List.cpp

---
+++ Remove_Nth_Node_From_end_Linked_List.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *temp=head;
+        ListNode* curr=head;
+        for(int i=0;i<n;i++){
+            curr=curr->next;
+        }
+        if(!curr){
+            return head->next;
+        }
+        while(curr->next){
+            curr=curr->next;
+            temp=temp->next;
+        }
+        temp->next=temp->next->next;
+        return head;
+    }
+};
