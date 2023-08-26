2f8f16d47308eaf13bbf75fdc73136b55acd718d
Sawan166 <114352257+Sawan166@users.noreply.github.com>
2023-08-26 23:52:52 +0530

    Create Merge_two_sorted_linked_list.cpp

---
+++ Merge_two_sorted_linked_list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* res = new ListNode(0);
+        ListNode* curr = res;
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                curr->next=list1;
+                list1=list1->next;
+            }
+            else{
+                curr->next=list2;
+                list2=list2->next;
+            }
+            curr=curr->next;
+        }
+        curr->next =list1? list1:list2;
+        return res->next;
+    }
+};
